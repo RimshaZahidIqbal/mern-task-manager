cacefcf56ee127e7ea3d1e918df7a8dd7e9c23f8
RimshaZahidIqbal <zrimsha219@gmail.com>
2025-04-28 00:20:46 +0500

    backend db + auth work half done

---
+++ backend/models/Task.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const todoSchema = new mongoose.Schema({
+    text: { text: String, required: true },
+    completed: { type: Boolean, default: false },
+});
+
+const taskSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    description: { type: String, enum: ["Low", "Medium", "High"], default: "Medium" },
+    status: { type: String, enum: ["Pending", "In Progress", "Completed"], default: "Pending" },
+    dueDate: { type: Date, required: true },
+    assignedTo: [{ type: mongoose.Schema.Types.ObjectId, ref: "User" }],
+    createdBy: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+    attachments: [{ type: String }],
+    toDoCheckList: [todoSchema],
+    progress: { type: Number, default: 0 },
+
+},
+    { timestamps: true });
+mongoose.exports = mongoose.model("Task", taskSchema);
