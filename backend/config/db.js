a568a9183e1a7e57ba32ea8544525ada4f0cd42a
RimshaZahidIqbal <zrimsha219@gmail.com>
2025-04-28 22:50:30 +0500

    db connected + users + admin registeration done

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,12 +1,16 @@
-const { default: mongoose } = require("mongoose");
-const mogoose = require("mongoose");
+const mongoose = require("mongoose");
+
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URI, {});
+        await mongoose.connect(process.env.MONGO_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
         console.log("MongoDB connected");
     } catch (err) {
-        console.log("Error connecting to MongoDB", err);
+        console.error("Error connecting to MongoDB", err);
         process.exit(1);
     }
 };
+
 module.exports = connectDB;
--- backend/server.js
+++ backend/server.js
@@ -2,33 +2,26 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const { connect } = require("http2");
 const connectDB = require("./config/db");
 
 const app = express();
 
-app.use(
-    cors({
-        origin: process.env.CLIENT_URL || "*",
-        methods: ["GET", "POST", "PUT", "DELETE"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-)
+const authRoutes = require("./routes/authRoutes");
 
 // Middlewares
-
+app.use(cors({
+    origin: process.env.CLIENT_URL || "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+}));
 app.use(express.json());
 
+// Connect to DB
 connectDB();
 
 // Routes
-// app.use('/api/auth',authRoutes);
-// app.use('/api/users',userRoutes); 
-// app.use('/api/tasks',taskRoutes);
-// app.use('/api/reports',reportRoutes);
+app.use('/api/auth', authRoutes);
 
-// StartServer
-
+// Start server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
-
