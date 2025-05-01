f7efa9a5c6ab9f6d0364cc8075524ea7823a6717
RimshaZahidIqbal <zrimsha219@gmail.com>
2025-05-02 01:01:22 +0500

    Login form done

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -5,7 +5,14 @@ export default {
         "./src/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                primary: '#1368EC',
+            },
+            fontFamily: {
+                display: ['"Noto Sans"', 'sans-serif'],
+            },
+        },
     },
     plugins: [],
 }
