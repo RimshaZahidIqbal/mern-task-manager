05291d02bfc2cbcb483f68835cdb5427f5a4dfa8
RimshaZahidIqbal <zrimsha219@gmail.com>
2025-05-04 16:56:08 +0500

    add side bar and nav bar done

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,8 +1,50 @@
-import React from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import { useUserAuth } from '../../Hooks/useUserAuth'
+import { UserContext } from '../../context/userContext';
+import { DashboardLayout } from '../../components/layouts';
+import { useNavigate } from 'react-router-dom';
+import axiosInstance from '../../utils/axiosInstance';
+import { API_PATHS } from '../../utils/apiPaths';
+import moment from "moment";
 
 const Dashboard = () => {
+    useUserAuth();
+    const { user } = useContext(UserContext);
+    const navigate = useNavigate();
+    const [dashboardData, setDashboardData] = useState(null);
+    const [pieChatData, setPieChatData] = useState(null);
+    const [barChartData, setBarChartData] = useState(null);
+
+    const getDashboardData = async () => {
+        try {
+            const response = await axiosInstance.get(
+                API_PATHS.TASKS.GET_DASHBOARD_DATA
+            );
+            if (response.data) {
+                setDashboardData(response.data);
+            }
+        } catch (error) {
+            console.error("Error fetching users:", error);
+        }
+    }
+
+    useEffect(() => {
+        getDashboardData();
+        return () => { };
+    }, []);
     return (
-        <div>Dashboard</div>
+        <DashboardLayout activeMenu="Dashboard">
+            <div className='card my-5'>
+                <div className="">
+                    <div className="col-span-3">
+                        <h2 className='text-xl md:text-2xl'>Greetings! {user.name}</h2>
+                        <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
+                            {moment().format("dddd Do MMM YYYY")}
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </DashboardLayout>
     )
 }
 
