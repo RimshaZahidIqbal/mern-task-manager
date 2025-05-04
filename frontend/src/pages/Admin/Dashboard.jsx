b194efd76687a7279103bbfb42edcb76fe3c1eef
RimshaZahidIqbal <zrimsha219@gmail.com>
2025-05-04 20:19:42 +0500

    dashboard top grid done

--- frontend/src/pages/Admin/Dashboard.jsx
+++ frontend/src/pages/Admin/Dashboard.jsx
@@ -1,12 +1,14 @@
 import React, { useContext, useEffect, useState } from 'react'
+import moment from "moment";
+import { useNavigate } from 'react-router-dom';
+import { IoMdCard } from "react-icons/io"
 import { useUserAuth } from '../../Hooks/useUserAuth'
 import { UserContext } from '../../context/userContext';
 import { DashboardLayout } from '../../components/layouts';
-import { useNavigate } from 'react-router-dom';
 import axiosInstance from '../../utils/axiosInstance';
 import { API_PATHS } from '../../utils/apiPaths';
-import moment from "moment";
-
+import InfoCard from '../../components/cards/InfoCard';
+import { addThousandsSeperator } from "../../utils/helper"
 const Dashboard = () => {
     useUserAuth();
     const { user } = useContext(UserContext);
@@ -37,11 +39,45 @@ const Dashboard = () => {
             <div className='card my-5'>
                 <div className="">
                     <div className="col-span-3">
-                        <h2 className='text-xl md:text-2xl'>Greetings! {user.name}</h2>
+                        <h2 className='text-xl md:text-2xl'>Greetings! {user?.name}</h2>
                         <p className='text-xs md:text-[13px] text-gray-400 mt-1.5'>
                             {moment().format("dddd Do MMM YYYY")}
                         </p>
                     </div>
+                </div>
+                <div className="grid grid-cols-2 sm:grid-cols-4 gap-3 md:gap-6 mt-5">
+                    <InfoCard
+                        // icon={<IoMdCard />}
+                        label="Total Tasks"
+                        value={addThousandsSeperator(
+                            dashboardData?.charts?.taskDistribution?.All || 0
+                        )}
+                        color="bg-primary"
+                    />
+                    <InfoCard
+                        // icon={<IoMdCard />}
+                        label="Pending Tasks"
+                        value={addThousandsSeperator(
+                            dashboardData?.charts?.taskDistribution?.Pending || 0
+                        )}
+                        color="bg-violet-500"
+                    />
+                    <InfoCard
+                        // icon={<IoMdCard />}
+                        label="In Progress Tasks"
+                        value={addThousandsSeperator(
+                            dashboardData?.charts?.taskDistribution?.InProgress || 0
+                        )}
+                        color="bg-cyan-500"
+                    />
+                    <InfoCard
+                        // icon={<IoMdCard />}
+                        label="Completed Tasks"
+                        value={addThousandsSeperator(
+                            dashboardData?.charts?.taskDistribution?.Completed || 0
+                        )}
+                        color="bg-lime-500"
+                    />
                 </div>
             </div>
         </DashboardLayout>
